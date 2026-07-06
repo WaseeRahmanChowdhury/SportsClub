30c368c54f9959003cb9c44a374fddf23725814b
Mainur Reza Mahi <mainurreza17@gmail.com>
2026-07-07 01:02:09 +0600

    updated receptionist and register visitor

---
+++ src/main/java/com/summer26/section1/group2/sportclub/mainur_reza_mahi/ReceptionistController.java
@@ -0,0 +1,46 @@
+package com.summer26.section1.group2.sportclub.mainur_reza_mahi;
+
+import javafx.event.ActionEvent;
+
+public class ReceptionistController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void dailyReportButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markExitButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void visitorLogButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registerVisitorButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewAppointmentButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageAppointmentButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchVisitorButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleAppointmentButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/summer26/section1/group2/sportclub/mainur_reza_mahi/RegisterVisitorController.java
@@ -0,0 +1,14 @@
+package com.summer26.section1.group2.sportclub.mainur_reza_mahi;
+
+import javafx.event.ActionEvent;
+
+public class RegisterVisitorController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void dailyReportButtonOA(ActionEvent actionEvent) {
+    }
+}
